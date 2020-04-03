6c7a586e1b09fee401c6381f042439eb0f607700
Aram Koukia <aramkoukia@gmail.com>
2020-04-03 12:08:50 -0700

    some refactoring

--- src/Controllers/CostController.cs
+++ src/Controllers/CostController.cs
@@ -16,26 +16,13 @@ namespace SaveOnCloudApi.Controllers
     {
         private readonly IConfiguration Configuration;
 
-        public CostController(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+        public CostController(IConfiguration configuration) => Configuration = configuration;
 
         [HttpGet]
         public IEnumerable<LabelValueModel> GetCost()
         {
-            // Create the consumption client and obtain data for the configured subscription
-            var credentials = SdkContext.AzureCredentialsFactory
-                        .FromServicePrincipal(Configuration["clientId"], Configuration["clientSecret"], Configuration["tenantId"], AzureEnvironment.AzureGlobalCloud);
-
-            using ConsumptionManagementClient consumptionClient = new ConsumptionManagementClient(credentials)
-            {
-                SubscriptionId = Configuration["SubscriptionId"]
-            };
-
-            //Get usage details and perform some basic processing
-            var result = ProcessUsageDetails(consumptionClient);
-            var data = result.SelectMany(kvp => kvp.Value).Distinct()
+            var usageDetail = GetUsageDetail();
+            return usageDetail.SelectMany(kvp => kvp.Value).Distinct()
                 .GroupBy(l => l.InstanceName)
                 .Select(cl => new LabelValueModel
                 {
@@ -43,25 +30,13 @@ namespace SaveOnCloudApi.Controllers
                     value = cl.Sum(c => c.PretaxCost).ToString(),
                 })
                 .OrderBy(o => o.value);
-
-            return data;
         }
 
         [HttpGet("daily")]
         public IEnumerable<DateValueModel> GetDailySpending()
         {
-            // Create the consumption client and obtain data for the configured subscription
-            var credentials = SdkContext.AzureCredentialsFactory
-                        .FromServicePrincipal(Configuration["clientId"], Configuration["clientSecret"], Configuration["tenantId"], AzureEnvironment.AzureGlobalCloud);
-
-            using ConsumptionManagementClient consumptionClient = new ConsumptionManagementClient(credentials)
-            {
-                SubscriptionId = Configuration["SubscriptionId"]
-            };
-
-            //Get usage details and perform some basic processing
-            var result = ProcessUsageDetails(consumptionClient);
-            var data = result.SelectMany(kvp => kvp.Value).Distinct()
+            var usageDetail = GetUsageDetail();
+            return usageDetail.SelectMany(kvp => kvp.Value).Distinct()
                 .GroupBy(l => l.UsageStart.Value.ToShortDateString())
                 .Select(cl => new DateValueModel
                 {
@@ -69,12 +44,26 @@ namespace SaveOnCloudApi.Controllers
                     value = cl.Sum(c => c.PretaxCost).ToString(),
                 })
                 .OrderBy(o => o.date);
+        }
 
-            return data;
+        [HttpGet("dayoverday")]
+        public IEnumerable<DateValueModel> GetDayOverDaySpending()
+        {
+            var usageDetail = GetUsageDetail();
+            return usageDetail.SelectMany(kvp => kvp.Value).Distinct()
+                .GroupBy(l => l.UsageStart.Value.ToShortDateString())
+                .Select(cl => new DateValueModel
+                {
+                    date = cl.First().UsageStart.Value.ToShortDateString(),
+                    value = cl.Sum(c => c.PretaxCost).ToString(),
+                })
+                .OrderBy(o => o.date);
         }
 
         [HttpGet("detail")]
-        public string GetDetail()
+        public string GetDetail() => JsonConvert.SerializeObject(GetUsageDetail());
+
+        private Dictionary<string, List<UsageDetail>> GetUsageDetail()
         {
             // Create the consumption client and obtain data for the configured subscription
             var credentials = SdkContext.AzureCredentialsFactory
@@ -84,26 +73,9 @@ namespace SaveOnCloudApi.Controllers
             {
                 SubscriptionId = Configuration["SubscriptionId"]
             };
-
-            //Get usage details and perform some basic processing
-            var result = ProcessUsageDetails(consumptionClient);
-            // GetFullPriceSheet(consumptionClient);
-
-            //Sum the cost of one of your resources based on usage detail cost info
-            // SumTotalCostOfAResource();
-
-            //Get the full price sheet
-            // GetFullPriceSheet(consumptionClient);
-
-            //Create a new sample budget
-            // CreateTestBudget(consumptionClient);
-            return JsonConvert.SerializeObject(result);
+            return ProcessUsageDetails(consumptionClient);
         }
 
-        /// <summary>
-        /// Method that queries and processes usage details.
-        /// </summary>
-        /// <param name="consumptionClient">The Consumption client.</param>
         private Dictionary<string, List<UsageDetail>> ProcessUsageDetails(ConsumptionManagementClient consumptionClient)
         {
             //Grab the usage details for this month
@@ -122,10 +94,6 @@ namespace SaveOnCloudApi.Controllers
             return result;
         }
 
-        /// <summary>
-        /// Method that process usage pages and places them into a generic list and dictionary by instanceId
-        /// </summary>
-        /// <param name="usagePage">The usage page</param>
         private Dictionary<string, List<UsageDetail>> ProcessUsagePage(IPage<UsageDetail> usagePage)
         {
             IEnumerator enumerator = usagePage.GetEnumerator();
@@ -152,96 +120,5 @@ namespace SaveOnCloudApi.Controllers
             }
             return usageDetailsByInstance;
         }
-
-        /// <summary>
-        /// Method that summarizes the cost of every usage detail record that pertains to one resource
-        /// </summary>
-        //private void SumTotalCostOfAResource()
-        //{
-        //    //Calculate the cost for the first usage detail resource in the general list
-        //    var sumCost = 0;
-        //    foreach (UsageDetail info in usageDetailsByInstance[usageDetails.First().InstanceId])
-        //    {
-        //        sumCost += info.PretaxCost;
-        //    }
-        //}
-
-        /// <summary>
-        /// Method that creates a test budget
-        /// </summary>
-        /// <param name="consumptionClient"></param>
-        //private void CreateTestBudget(ConsumptionManagementClient consumptionClient)
-        //{
-        //    //Create a new budget object with the calculated cost above
-        //    Budget newBudget = new Budget
-        //    {
-        //        Amount = (decimal)sumCost,
-        //        Category = "Cost",
-        //        Filters = new Filters
-        //        {
-        //            Resources = new List<string>
-        //            {
-        //                usageDetails.First().InstanceId
-        //            }
-        //        },
-        //        TimeGrain = "Monthly",
-        //        TimePeriod = new BudgetTimePeriod
-        //        {
-        //            StartDate = DateTime.UtcNow.AddDays(-DateTime.UtcNow.Day + 1),
-        //            EndDate = DateTime.UtcNow.AddMonths(2)
-        //        }
-
-        //        /* Notifications for this budget can also be configured, uncomment the code below to add one
-        //        ,Notifications = new Dictionary<string, Notification>()
-        //        {
-        //            {
-        //                "NotificationName", new Notification
-        //                {
-        //                    Enabled = true,
-        //                    Threshold = (decimal)0.90,
-        //                    ContactEmails = new List<string> { "YOUREMAIL@EMAIL.com"},
-        //                    ContactGroups = null,
-        //                    ContactRoles = null,
-        //                    OperatorProperty = "GreaterThan"
-        //                }
-        //            }
-        //        }
-        //        */
-        //    };
-
-        //    //Create the budget
-        //    consumptionClient.Budgets.CreateOrUpdate(
-        //        budgetName,
-        //        newBudget);
-        //}
-
-        /// <summary>
-        /// Method that queries to obtain the full price sheet.
-        /// </summary>
-        /// <param name="consumptionClient"></param>
-        //private static void GetFullPriceSheet(ConsumptionManagementClient consumptionClient)
-        //{
-        //    var priceSheet = new List<PriceSheetProperties>();
-        //    //Get price first price sheet result and put the properties into a list
-        //    PriceSheetResult priceSheetResult = consumptionClient.PriceSheet.Get();
-        //    foreach (PriceSheetProperties properties in priceSheetResult.Pricesheets)
-        //    {
-        //        priceSheet.Add(properties);
-        //    }
-
-        //    //Process subsequest price sheet results
-        //    while (priceSheetResult.NextLink != "")
-        //    {
-        //        Uri nextPriceSheetLink = new Uri(priceSheetResult.NextLink);
-        //        var query = HttpUtility.ParseQueryString(nextPriceSheetLink.Query);
-        //        string skipToken = query.Get("$skiptoken");
-
-        //        priceSheetResult = consumptionClient.PriceSheet.Get(null, skipToken);
-        //        foreach (PriceSheetProperties properties in priceSheetResult.Pricesheets)
-        //        {
-        //            priceSheet.Add(properties);
-        //        }
-        //    }
-        //}
     }
 }
