ce5ad85bc10d75b72dd90744c640214f01317c99
Aram Koukia <aramkoukia@gmail.com>
2020-04-12 16:05:26 -0700

    adding Subscription controller

---
+++ src/Controllers/SubscriptionController.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SaveOnCloudApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SubscriptionController : ControllerBase
+    {
+
+        [AllowAnonymous]
+        [HttpPost]
+        public async Task<IActionResult> PostAsync(object model)
+        {
+            return await Task.FromResult<IActionResult>(Ok());
+
+        }
+    }
+}
