76ae5eb0c7d6f2106d8fe3d1196387d4d8e81946
Aram Koukia <aramkoukia@gmail.com>
2020-04-17 13:01:19 -0700

    removed v2

--- v2/Contoso.Application/Students/Queries/GetAllStudents.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-using SaveOnCloud.Core.Domain;
-using MediatR;
-
-namespace SaveOnCloud.Application.Students.Queries
-{
-    public class GetAllStudents : IRequest<List<StudentViewModel>>
-    { }
-}
